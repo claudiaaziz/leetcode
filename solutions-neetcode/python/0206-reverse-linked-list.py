9aacd6036cf5e3930a32d6c81fbb540bf1ea4bd6
Harrison Liang <liangharrison123@gmail.com>
2024-01-24 16:18:52 -0500

    206

--- solutions-neetcode/python/0206-reverse-linked-list.py
+++ solutions-neetcode/python/0206-reverse-linked-list.py
@@ -15,3 +15,17 @@ class Solution:
             prev = curr
             curr = temp
         return prev
+
+
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, curr = None, head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        
+        return prev
