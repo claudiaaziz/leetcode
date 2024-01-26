35a4f7daefe2c0de7a8d14c96f431359c0d1b125
Harrison Liang <liangharrison123@gmail.com>
2024-01-26 17:50:24 -0500

    138

--- solutions-neetcode/python/0138-copy-list-with-random-pointer.py
+++ solutions-neetcode/python/0138-copy-list-with-random-pointer.py
@@ -24,3 +24,31 @@ class Solution:
             copy.random = oldToCopy[cur.random]
             cur = cur.next
         return oldToCopy[head]
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # Dictionary to map old nodes to their corresponding new nodes
+        old_to_copy = {None: None}
+
+        # First pass: Create new nodes and build the mapping
+        cur = head
+        while cur:
+            # Create a new node with the same value
+            copy = Node(cur.val)
+            # Map the old node to the new node
+            old_to_copy[cur] = copy
+            cur = cur.next
+
+        # Second pass: Connect the new nodes' next and random pointers
+        cur = head
+        while cur:
+            # Get the corresponding new node for the current old node
+            copy = old_to_copy[cur]
+            # Connect the next pointer
+            copy.next = old_to_copy[cur.next]
+            # Connect the random pointer
+            copy.random = old_to_copy[cur.random]
+            cur = cur.next
+
+        # Return the head of the copied linked list, returns the values which are copied
+        return old_to_copy[head]
