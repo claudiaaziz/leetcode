d110139a5066cccd9b408c885c287a9824f3df6f
Harrison Liang <liangharrison123@gmail.com>
2024-01-26 18:02:31 -0500

    876

--- solutions-neetcode/python/0876-middle-of-the-linked-list.py
+++ solutions-neetcode/python/0876-middle-of-the-linked-list.py
@@ -8,3 +8,13 @@ class Solution:
             slow, fast = slow.next, fast.next.next
 
         return slow
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
