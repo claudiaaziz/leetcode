49b3005ec5caffe694cb2da277a79d2cd9d722fa
Harrison Liang <liangharrison123@gmail.com>
2024-01-30 19:57:27 -0500

    203

--- solutions-neetcode/python/0203-remove-linked-list-elements.py
+++ solutions-neetcode/python/0203-remove-linked-list-elements.py
@@ -13,3 +13,23 @@ class Solution:
             
             curr = nxt
         return dummy.next
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode()
+        dummy.next = head
+        curr = dummy
+
+        while curr and curr.next:
+            if curr.next.val == val:
+                curr.next = curr.next.next
+            else:
+                curr = curr.next 
+        
+        return dummy.next
