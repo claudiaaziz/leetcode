4d79b55ed11c7883fd8e0dc66791926dbf8d125e
Harrison Liang <liangharrison123@gmail.com>
2024-02-01 18:51:04 -0500

    424

--- solutions-neetcode/python/0424-longest-repeating-character-replacement.py
+++ solutions-neetcode/python/0424-longest-repeating-character-replacement.py
@@ -13,3 +13,25 @@ class Solution:
                 l += 1
 
         return (r - l + 1)
+
+
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        max_s = 0
+        left = 0
+
+        for right in range(len(s)):
+            if s[right] in count:
+                count[s[right]]+=1
+            else:
+                count[s[right]] = 1
+            
+            while (right - left + 1) - max(count.values()) > k:
+                count[s[left]]-=1
+                left+=1
+            
+            max_s = max(max_s, right - left + 1)
+
+        return max_s
