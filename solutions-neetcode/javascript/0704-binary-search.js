33b033b3d19eeab5881fab604bf4f1b19e1bcba9
claudiaaziz <claudiahannahh@gmail.com>
2024-02-03 09:12:00 -0500

    704

--- solutions-neetcode/javascript/0704-binary-search.js
+++ solutions-neetcode/javascript/0704-binary-search.js
@@ -23,3 +23,21 @@ var search = function (nums, target) {
 
     return -1;
 };
+
+// claudia
+// time o(log n) - space o(log n)  
+const search = (nums, target) => {
+    if (nums.length === 0) return -1
+    const midIdx = Math.floor(nums.length / 2) 
+    const left = nums.slice(0, midIdx)
+    const right = nums.slice(midIdx + 1)
+
+    if (nums[midIdx] === target) { 
+        return midIdx
+    } else if (nums[midIdx] > target) {
+        return search(left, target)
+    } else {
+        const rightRes = search(right, target)
+        return rightRes === -1 ? - 1 : rightRes + midIdx + 1
+    }
+};
