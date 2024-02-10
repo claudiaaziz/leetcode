ac35ad28076e8686c5a54c670d18d8783a5d350d
claudiaaziz <claudiahannahh@gmail.com>
2024-02-10 09:24:47 -0500

    21

--- solutions-neetcode/javascript/0021-merge-two-sorted-lists.js
+++ solutions-neetcode/javascript/0021-merge-two-sorted-lists.js
@@ -58,3 +58,30 @@ var mergeTwoLists = function(list1, list2) {
 
     return sentinel.next;
 };
+
+// claudia
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+
+// time o(n + m) || space o(n * k)
+var mergeTwoLists = function(list1, list2) {
+    const tempNode = new ListNode(0, null)
+    let currentNode = tempNode
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            currentNode.next = list1
+            list1 = list1.next
+        } else {
+            currentNode.next = list2
+            list2 = list2.next
+        }
+        currentNode = currentNode.next
+    }
+
+    currentNode.next = list1 || list2
+    return tempNode.next
+};
