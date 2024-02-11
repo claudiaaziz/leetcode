a5fd600f691dfd5edafa85cadacf3fb1d6007193
Harrison Liang <liangharrison123@gmail.com>
2024-02-10 23:24:08 -0500

    572

--- solutions-neetcode/python/0572-subtree-of-another-tree.py
+++ solutions-neetcode/python/0572-subtree-of-another-tree.py
@@ -21,3 +21,34 @@ class Solution:
         if s and t and s.val == t.val:
             return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
         return False
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSameTree(p, q):
+            #base case
+            if p is None and q is None:
+                return True
+            if (p is None or q is None) or (p.val != q.val):
+                return False
+        
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+
+        
+        if not subRoot: return True
+        if not root: return False
+
+        if isSameTree(root, subRoot):
+            return True
+        
+        return (self.isSubtree(root.left, subRoot) or 
+                self.isSubtree(root.right, subRoot))
