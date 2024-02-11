d68e10b0c5cf7a075ed1ba9612db4743132b60f4
Harrison Liang <liangharrison123@gmail.com>
2024-02-10 23:25:49 -0500

    543

--- solutions-neetcode/python/0543-diameter-of-binary-tree.py
+++ solutions-neetcode/python/0543-diameter-of-binary-tree.py
@@ -21,3 +21,41 @@ class Solution:
 
         dfs(root)
         return res
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        diameter = 0
+        
+        #returns the height for the current node
+        def height(root):
+            nonlocal diameter
+            
+            if not root:
+                return 0
+            
+            #returns the max height on the left side
+            left = height(root.left)
+            #returns the max height on the right side
+            right = height(root.right)
+
+            #updates the max diameter by adding both sides
+            diameter = max(diameter, left + right)
+
+            curr_height = max(left, right) + 1
+
+            return curr_height
+        
+        height(root)
+
+        return diameter
+
