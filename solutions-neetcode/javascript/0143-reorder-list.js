d8105595e0a2c462e210da95f45fdbfffe5e19d9
claudiaaziz <claudiahannahh@gmail.com>
2024-02-11 10:41:16 -0500

    143

--- solutions-neetcode/javascript/0143-reorder-list.js
+++ solutions-neetcode/javascript/0143-reorder-list.js
@@ -49,3 +49,52 @@ const reorder = (l1, l2) => {
         second = next;
     }
 }
+
+// claudia
+// time o(n/2) - space o(1)
+const reorderList = (head) => {
+    // PART 1 - find head2
+    let slow = head;
+    let fast = head;
+
+    while (fast !== null && fast.next !== null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    let head2 = slow.next;
+    slow.next = null;
+
+    // PART 2 - reverse head2
+    const reversedHead2 = reverseHead2(head2);
+
+    // PART 3 - zip lists
+    zipLists(head, reversedHead2)
+};
+
+const reverseHead2 = (head2) => {
+    let current = head2;
+    let prev = null;
+
+    while (current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+};
+
+const zipLists = (h1, h2) => {
+    while (h2 !== null) {
+        const temp1 = h1.next;
+        const temp2 = h2.next;
+
+        h1.next = h2;
+        h2.next = temp1;
+
+        h1 = temp1;
+        h2 = temp2;
+    }
+}
