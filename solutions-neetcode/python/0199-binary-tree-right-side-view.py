7911747d793b5a6a268b6d2d84ca8049fdabd8fe
Harrison Liang <liangharrison123@gmail.com>
2024-02-15 10:48:20 -0500

    199

--- solutions-neetcode/python/0199-binary-tree-right-side-view.py
+++ solutions-neetcode/python/0199-binary-tree-right-side-view.py
@@ -22,3 +22,33 @@ class Solution:
             if rightSide:
                 res.append(rightSide.val)
         return res
+    
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None: return []
+
+        queue = deque([root])
+        res = []
+
+        while queue:
+            level_length = len(queue)
+
+            for i in range(level_length):
+                curr = queue.popleft()
+
+                if i == level_length - 1: res.append(curr.val)
+
+                if curr.left: queue.append(curr.left)
+                if curr.right: queue.append(curr.right)
+            
+        return res
