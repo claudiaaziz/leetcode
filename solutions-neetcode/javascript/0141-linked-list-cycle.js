e08db4e0612e6f8a1b61a14207b16e7751c9e85e
claudiaaziz <claudiahannahh@gmail.com>
2024-02-15 09:39:26 -0500

    141

--- solutions-neetcode/javascript/0141-linked-list-cycle.js
+++ solutions-neetcode/javascript/0141-linked-list-cycle.js
@@ -34,3 +34,18 @@ var hasCycle = function(head) {
 
     return false;
 };
+
+// claudia
+// t o(n) - space o(1)
+const hasCycle = head => {
+    let slow = head
+    let fast = head
+
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
+        if (fast === slow) return true
+    }
+
+    return false
+};
