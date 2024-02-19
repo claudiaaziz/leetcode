5ee2ae69e206bdd52a51f35d1ac44761d7341121
Harrison Liang <liangharrison123@gmail.com>
2024-02-19 18:40:45 -0500

    208

--- solutions-neetcode/python/0268-missing-number.py
+++ solutions-neetcode/python/0268-missing-number.py
@@ -5,3 +5,15 @@ class Solution:
         for i in range(len(nums)):
             res += i - nums[i]
         return res
+    
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        total = 0
+        #sum range from 0 to length of nums + 1
+        for i in range(0, len(nums) + 1):
+            total+=i
+        #sum nums
+        #find difference
+        return total - sum(nums)
