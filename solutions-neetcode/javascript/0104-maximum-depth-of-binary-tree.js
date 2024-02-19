61fe3a8f56fa8ab73eadc68196375b0c51298bb4
claudiaaziz <claudiahannahh@gmail.com>
2024-02-19 11:01:09 -0500

    104

--- solutions-neetcode/javascript/0104-maximum-depth-of-binary-tree.js
+++ solutions-neetcode/javascript/0104-maximum-depth-of-binary-tree.js
@@ -73,3 +73,32 @@ const bfs = (queue, height = 0) => {
 
     return height;
 };
+
+// t o(n) - s o(n)
+// var maxDepth = function(root) {
+//     if (!root) return 0
+//     const left = maxDepth(root.left)
+//     const right = maxDepth(root.right)
+//     return Math.max(left, right) + 1
+// };
+
+// t o(n) - s o(n)
+var maxDepth = function(root) {
+    if (!root) return 0
+    let depth = 0
+    const queue = [ root ]
+
+    while (queue.length) {
+        const len = queue.length
+
+        for (let i = 0; i < len; i++) {
+            const current = queue.shift()  
+            if (current.left) queue.push(current.left)
+            if (current.right) queue.push(current.right)
+        }
+        
+        depth++
+    }
+
+    return depth
+};
