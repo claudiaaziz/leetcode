e78897ccf0119696b249e076e8d33631908703d1
Harrison Liang <liangharrison123@gmail.com>
2024-02-23 18:17:48 -0500

    46

--- solutions-neetcode/python/0046-permutations.py
+++ solutions-neetcode/python/0046-permutations.py
@@ -15,3 +15,26 @@ class Solution:
             res.extend(perms)
             nums.append(n)
         return res
+
+
+
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        combinations = []
+
+        def dfs(current_permutation, remaining_numbers):
+            if remaining_numbers == []:
+                combinations.append(current_permutation.copy())
+                return
+            
+            for i in range(len(remaining_numbers)):
+                current_permutation.append(remaining_numbers[i])
+
+                dfs(current_permutation, remaining_numbers[0:i] + remaining_numbers[i+1:])
+
+                current_permutation.pop()
+        
+        dfs([], nums)
+
+        return combinations
