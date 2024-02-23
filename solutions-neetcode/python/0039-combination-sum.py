1191f3a4256d8ee3793b7d69453c3aa5d1cc19bc
Harrison Liang <liangharrison123@gmail.com>
2024-02-23 18:17:28 -0500

    39

--- solutions-neetcode/python/0039-combination-sum.py
+++ solutions-neetcode/python/0039-combination-sum.py
@@ -16,3 +16,38 @@ class Solution:
 
         dfs(0, [], 0)
         return res
+    
+
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        #where all the combinations will go
+        res = []
+
+        #recursive function: takes in index, combination, total so far
+        def dfs(i, sublist, total):
+            #base case 1: we found a combination that sums to the target
+            if total == target:
+                res.append(sublist.copy())
+                return
+            
+            #base case 2: combination is too big or we went through the entire candidates
+            #list
+            if total > target or i >= len(candidates):
+                return
+            
+            #choice 1: include the current candidate, repeats allowed
+            sublist.append(candidates[i])
+            #we stay at the current candidate and we need to update total
+            dfs(i, sublist, total + candidates[i])
+
+            #choice 2: we don't include current candidate
+            sublist.pop()
+            #we move onto a new candidate and total remains the same
+            dfs(i + 1, sublist, total)
+        
+        #start off at index 0, empty sublist, 0 total
+        dfs(0, [], 0)
+
+        return res
