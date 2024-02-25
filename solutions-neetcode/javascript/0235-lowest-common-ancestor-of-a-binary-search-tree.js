96877800db7de42d919ff6aa06da059f9f4228cd
claudiaaziz <claudiahannahh@gmail.com>
2024-02-25 10:08:25 -0500

    235

--- solutions-neetcode/javascript/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ solutions-neetcode/javascript/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -43,3 +43,15 @@
 
     return root;
 };
+
+// claudia 
+// t o(n) - space o(h)
+const lowestCommonAncestor = (root, p, q) => {
+    if (p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q)
+    } else if (p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q)
+    } else {
+        return root
+    }
+};
