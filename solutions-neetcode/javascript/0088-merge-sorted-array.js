c439be99965b21c06d604d65c095fe15382a2153
claudiaaziz <claudiahannahh@gmail.com>
2024-03-04 13:22:26 -0500

    88

--- solutions-neetcode/javascript/0088-merge-sorted-array.js
+++ solutions-neetcode/javascript/0088-merge-sorted-array.js
@@ -32,3 +32,22 @@ var merge = function(nums1, m, nums2, n) {
         }
     }
 };
+
+// claudia
+// t o(n+m) - s o(1)
+var merge = function(nums1, m, nums2, n) {
+    let i = nums1.length-1
+    m--
+    n--
+
+    while (n >= 0) {
+        if (nums1[m] > nums2[n]) {
+            nums1[i] = nums1[m]
+            m--
+        } else {
+            nums1[i] = nums2[n]
+            n--
+        }
+        i--
+    }
+};
