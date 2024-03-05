4924b8b62c7cf6b221143d2222bd8bb075dda3c1
Harrison Liang <liangharrison123@gmail.com>
2024-03-05 11:36:44 -0500

    1046

--- solutions-neetcode/python/1046-last-stone-weight.py
+++ solutions-neetcode/python/1046-last-stone-weight.py
@@ -27,3 +27,28 @@ class Solution(object):
         
         stones.append(0)
         return stones[0]
+
+
+
+
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        #make them all negative because we are trying to do max heap
+        stones = [-stone for stone in stones]
+        heapq.heapify(stones)
+
+        #keep going until there's 1 or no stones left
+        while len(stones) > 1:
+            #get the first two, make them positive
+            first = abs(heapq.heappop(stones))
+            second = abs(heapq.heappop(stones))
+
+            #if the second one is smaller, find the negative difference
+            #and add it to the heap
+            if second < first: heapq.heappush(stones, second - first)
+        
+        #add 0 at the end and return the first element
+        stones.append(0)
+        return abs(stones[0])
