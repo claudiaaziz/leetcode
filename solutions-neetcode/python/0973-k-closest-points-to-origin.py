b744815144cf1f8d796d5aa9d169a5bc3bd665ac
Harrison Liang <liangharrison123@gmail.com>
2024-03-05 11:37:08 -0500

    973

--- solutions-neetcode/python/0973-k-closest-points-to-origin.py
+++ solutions-neetcode/python/0973-k-closest-points-to-origin.py
@@ -11,3 +11,30 @@ class Solution:
             _, x, y = heapq.heappop(minHeap)
             res.append((x, y))
         return res
+
+
+
+
+
+import heapq
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        #create a list to store distance, x, and y
+        minHeap = []
+        for x, y in points:
+            distance = (x**2) + (y**2)
+            minHeap.append([distance, x, y])
+        
+        #turn the list into a heap
+        heapq.heapify(minHeap)
+        
+        #pop from the heap the minimums and append [x, y] to result
+        res = []
+        while k > 0:
+            distance, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k-=1
+        
+        #return result
+        return res
