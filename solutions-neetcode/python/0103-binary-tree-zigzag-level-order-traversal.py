fefd226298811bfb03c55f9da80e6b88cd7d3dae
Harrison Liang <liangharrison123@gmail.com>
2024-03-06 17:32:50 -0500

    103

--- solutions-neetcode/python/0103-binary-tree-zigzag-level-order-traversal.py
+++ solutions-neetcode/python/0103-binary-tree-zigzag-level-order-traversal.py
@@ -22,3 +22,49 @@ class Solution:
             result.append(level[::zigzagDirection])
             zigzagDirection *= -1
         return result
+    
+
+
+
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        #edge case
+        if root is None: return []
+
+        #queue to keep track of the nodes
+        queue = deque([root])
+
+        #result list to keep track of each level
+        res = []
+
+        while queue:
+            #sublist to keep track of the values on each level
+            sublist = []
+
+            #keep track of the length of the current level
+            curr_q_len = len(queue)
+
+            for i in range(curr_q_len):
+                popped_ele = queue.popleft()
+                sublist.append(popped_ele.val)
+
+                if popped_ele.left:
+                    queue.append(popped_ele.left)
+                if popped_ele.right:
+                    queue.append(popped_ele.right)
+
+            if len(res) % 2 == 0:
+                res.append(sublist)
+            else:
+                res.append(sublist[::-1])
+
+        return res
