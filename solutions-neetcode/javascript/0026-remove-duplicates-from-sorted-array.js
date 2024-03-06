e4d54b3991636fde546a6fb074dc0916b0ccefb3
claudiaaziz <claudiahannahh@gmail.com>
2024-03-06 09:26:23 -0500

    26

--- solutions-neetcode/javascript/0026-remove-duplicates-from-sorted-array.js
+++ solutions-neetcode/javascript/0026-remove-duplicates-from-sorted-array.js
@@ -22,3 +22,22 @@ var removeDuplicates = (nums) => {
 
     return (left + 1);
 };
+
+// claudia
+// t o(n) - s o(k)
+var removeDuplicates = function(nums) {
+    const seen = new Set()
+    let l = 0
+    let r = 0
+
+    while (r < nums.length) {
+        if (!seen.has(nums[r])) {
+            seen.add(nums[r])
+            nums[l] = nums[r]
+            l++
+        }
+        r++
+    }
+
+    return l
+};
