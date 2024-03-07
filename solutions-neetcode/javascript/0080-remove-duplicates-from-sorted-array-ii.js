8e54b8686b10e53cc89afa9af0d71dec3872094d
claudiaaziz <claudiahannahh@gmail.com>
2024-03-07 09:34:51 -0500

    80

--- solutions-neetcode/javascript/0080-remove-duplicates-from-sorted-array-ii.js
+++ solutions-neetcode/javascript/0080-remove-duplicates-from-sorted-array-ii.js
@@ -1,50 +1,67 @@
 /**
-* https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
-* 
-* Time O(n) | Space O(1)
-* @param {number[]} nums
-* @return {number}
-*/
-var removeDuplicates = function(nums) {
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ *
+ * Time O(n) | Space O(1)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
   let current = nums[0];
   let sameElCount = 0;
 
-  for(let i = 0; i < nums.length; i++) {
-    if(current === nums[i]) {
-        sameElCount++;
-    } 
-    if(current !== nums[i]) {
-        current = nums[i];
-        sameElCount = 1;
+  for (let i = 0; i < nums.length; i++) {
+    if (current === nums[i]) {
+      sameElCount++;
     }
-   if(sameElCount > 2) {
-       nums.splice(i,1);
-       i--;
+    if (current !== nums[i]) {
+      current = nums[i];
+      sameElCount = 1;
+    }
+    if (sameElCount > 2) {
+      nums.splice(i, 1);
+      i--;
     }
   }
 };
 
-
 /**
-* Two pointer
-* Time O(n^2) | Space O(1)
-* @param {number[]} nums
-* @return {number}
-*/
-var removeDuplicates2 = function(nums) {
-    const isEdgeCase = (nums.length < 2)
-    if (isEdgeCase) return nums.length;
+ * Two pointer
+ * Time O(n^2) | Space O(1)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates2 = function (nums) {
+  const isEdgeCase = nums.length < 2;
+  if (isEdgeCase) return nums.length;
 
-    let [ left, right ] = [ 2, 2 ];
+  let [left, right] = [2, 2];
 
-    while (right < nums.length) {/* Time O(N) */
-        const isEqual = (nums[(left - 2)] === nums[right]);
-        if (!isEqual) {
-            nums[left] = nums[right];
-            left += 1;
-        }
-
-        right += 1;
+  while (right < nums.length) {
+    /* Time O(N) */
+    const isEqual = nums[left - 2] === nums[right];
+    if (!isEqual) {
+      nums[left] = nums[right];
+      left += 1;
     }
-    return left;
+
+    right += 1;
+  }
+  return left;
+};
+
+// claudia
+// t o(n) - s o(1) 
+var removeDuplicates = function(nums) {
+    if (nums.length < 3) return nums.length
+
+    let j = 2
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[j-2] !== nums[i] || nums[j-1] !== nums[i]) {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+
+    return j
 };
