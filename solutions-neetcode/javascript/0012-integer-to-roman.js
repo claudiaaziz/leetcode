bdd487e44921dea7721310a7a31e683784afaf43
claudiaaziz <claudiahannahh@gmail.com>
2024-03-24 10:43:49 -0400

    12

--- solutions-neetcode/javascript/0012-integer-to-roman.js
+++ solutions-neetcode/javascript/0012-integer-to-roman.js
@@ -82,3 +82,20 @@ var intToRoman = function(num) {
     }
     return ans;
 };
+
+// claudia
+// t o(n) - s o(1)
+var intToRoman = function(num) {
+    const val = [1000,900,500,400,100,90,50,40,10,9,5,4,1];
+    const rom = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
+    let res = ""
+
+    for (let i = 0; num; i++) {
+        while (num >= val[i]) {
+            res += rom[i]
+            num -= val[i]
+        }
+    }
+
+    return res
+};
