02b55bc269d7a762aad9b166879e2b9a4149649b
Harrison Liang <liangharrison123@gmail.com>
2024-03-27 19:57:53 -0400

    75

--- solutions-neetcode/python/0075-sort-colors.py
+++ solutions-neetcode/python/0075-sort-colors.py
@@ -15,3 +15,23 @@ class Solution:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
         return nums
+    
+
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        counts = [0, 0, 0]
+
+        for num in nums:
+            counts[num] += 1
+        
+        i_nums = 0
+        for idx, color_count in enumerate(counts):
+            for j in range(color_count):
+                nums[i_nums] = idx
+                i_nums += 1
+        
