41a3ff3e06d7ea176a223d40582b1961ac1b2926
Harrison Liang <liangharrison123@gmail.com>
2024-04-01 17:15:57 -0400

    70

--- solutions-neetcode/python/0070-climbing-stairs.py
+++ solutions-neetcode/python/0070-climbing-stairs.py
@@ -9,3 +9,23 @@ class Solution:
             n1 = n2
             n2 = temp
         return n2
+
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def dfs(n, memo):
+            if n in memo:
+                return memo[n]
+            if n == 0:
+                return 1
+            if n < 0:
+                return 0
+            
+            one_step = dfs(n - 1, memo)
+            two_steps = dfs(n - 2, memo)
+
+            memo[n] = one_step + two_steps
+            return one_step + two_steps
+        
+        return dfs(n, {})
