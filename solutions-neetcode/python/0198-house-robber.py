a4cad58b08be864dd88f88a31e03e539459cbf3f
Harrison Liang <liangharrison123@gmail.com>
2024-04-01 17:16:48 -0400

    198

--- solutions-neetcode/python/0198-house-robber.py
+++ solutions-neetcode/python/0198-house-robber.py
@@ -7,3 +7,26 @@ class Solution:
             rob1 = rob2
             rob2 = temp
         return rob2
+
+
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def dfs(i, memo):
+            if i in memo:
+                return memo[i]
+            if i >= len(nums):
+                return 0
+
+            include = nums[i] + dfs(i + 2, memo)
+            exclude = dfs(i + 1, memo)
+            
+            memo[i] = max(include, exclude)
+            return max(include, exclude)
+        
+        return dfs(0, {})
+
+
+        
