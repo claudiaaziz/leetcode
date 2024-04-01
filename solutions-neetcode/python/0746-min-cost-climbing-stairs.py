18329fa43425c45ed5afa13d2973c2f230b4b7f7
Harrison Liang <liangharrison123@gmail.com>
2024-04-01 17:16:28 -0400

    746

--- solutions-neetcode/python/0746-min-cost-climbing-stairs.py
+++ solutions-neetcode/python/0746-min-cost-climbing-stairs.py
@@ -4,3 +4,27 @@ class Solution:
             cost[i] += min(cost[i + 1], cost[i + 2])
 
         return min(cost[0], cost[1])
+
+
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        last_idx = len(cost)
+
+        def dfs(i, memo):
+            if i in memo:
+                return memo[i]
+            if i >= last_idx:
+                return 0
+            
+            one_step = cost[i] + dfs(i + 1, memo)
+            two_steps = cost[i] + dfs(i + 2, memo)
+
+            memo[i] = min(one_step, two_steps)
+            return min(one_step, two_steps)
+        
+        return min(dfs(0, {}), dfs(1, {}))
+
+
+        
