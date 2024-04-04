501eeb4ad5ddb925fc8925c286da375af3b7ede0
Harrison Liang <liangharrison123@gmail.com>
2024-04-04 11:42:53 -0400

    53

--- solutions-neetcode/python/0053-maximum-subarray.py
+++ solutions-neetcode/python/0053-maximum-subarray.py
@@ -9,3 +9,22 @@ class Solution:
             if total < 0:
                 total = 0
         return res
+
+
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum = nums[0]
+        curr_sum = 0
+
+        for num in nums:
+            # If the current sum is negative, we can reset it because it will
+            # cause us to get a smaller number.
+            if curr_sum < 0:
+                curr_sum = 0
+            curr_sum += num
+            max_sum = max(max_sum, curr_sum)
+        
+        return max_sum
+        
