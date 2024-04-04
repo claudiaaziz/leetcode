5658537c414fed533eec42c9cd3984270d09fb2d
Harrison Liang <liangharrison123@gmail.com>
2024-04-04 11:43:19 -0400

    647

--- solutions-neetcode/python/0647-palindromic-substrings.py
+++ solutions-neetcode/python/0647-palindromic-substrings.py
@@ -14,3 +14,36 @@ class Solution:
             l -= 1
             r += 1
         return res
+
+
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+
+        for i in range(len(s)):
+            # Odd 
+            left = i
+            right = i
+            count += self.helper(left, right, s)
+            
+            # Even
+            left = i
+            right = i + 1
+            count += self.helper(left, right, s)
+
+        return count
+    
+    def helper(self, pt1, pt2, string):
+        res = 0
+
+        while pt1 >= 0 and pt2 < len(string) and string[pt1] == string[pt2]:
+            res += 1
+            pt1 -= 1
+            pt2 += 1
+        
+        return res
+
+
+
+        
