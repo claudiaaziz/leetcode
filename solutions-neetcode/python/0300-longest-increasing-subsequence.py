bbd7c4a9e1b767016cd439f02cccc7985017781e
Harrison Liang <liangharrison123@gmail.com>
2024-04-04 11:44:59 -0400

    300:

--- solutions-neetcode/python/0300-longest-increasing-subsequence.py
+++ solutions-neetcode/python/0300-longest-increasing-subsequence.py
@@ -7,3 +7,15 @@ class Solution:
                 if nums[i] < nums[j]:
                     LIS[i] = max(LIS[i], 1 + LIS[j])
         return max(LIS)
+    
+
+
+class Solution:  # 2516 ms, faster than 64.96%
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
