48fd4237378405a973c50d354224e023a81c36e2
Harrison Liang <liangharrison123@gmail.com>
2024-04-04 11:44:03 -0400

    322

--- solutions-neetcode/python/0322-coin-change.py
+++ solutions-neetcode/python/0322-coin-change.py
@@ -8,3 +8,31 @@ class Solution:
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a - c])
         return dp[amount] if dp[amount] != amount + 1 else -1
+    
+
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+
+        def dfs(amount, memo):
+            if amount in memo:
+                return memo[amount]
+            if amount == 0:
+                return 0
+            if amount < 0:
+                return float("inf")
+
+            min_change = float("inf")
+            for coin in coins:
+                min_change = min(min_change, 1 + dfs(amount - coin, memo))
+            
+            memo[amount] = min_change
+            return min_change
+        
+        answer = dfs(amount, {})
+        if answer == float("inf"):
+            return -1
+        else:
+            return answer
+        
