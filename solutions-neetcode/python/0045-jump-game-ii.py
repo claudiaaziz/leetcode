cea60b261396b5eeb7cd7ea9a975014601f5db73
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:12:53 -0400

    45

--- solutions-neetcode/python/0045-jump-game-ii.py
+++ solutions-neetcode/python/0045-jump-game-ii.py
@@ -10,3 +10,23 @@ class Solution:
             r = maxJump
             res += 1
         return res
+    
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # Keep a window size of each step. If any of the numbers in the window reaches
+        # the last index, we're done and can return count.
+        left = right = 0
+        count = 0
+
+        farthest = 0
+        while right < len(nums) - 1:
+            for i in range(left, right + 1):
+                farthest = max(farthest, i + nums[i])
+            left = right + 1
+            right = farthest
+            count+=1
+        
+        return count
+            
+        
