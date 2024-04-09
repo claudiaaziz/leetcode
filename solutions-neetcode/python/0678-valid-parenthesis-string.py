66c8eb393029a68aa8500a129f97b91852012d8e
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:15:41 -0400

    678

--- solutions-neetcode/python/0678-valid-parenthesis-string.py
+++ solutions-neetcode/python/0678-valid-parenthesis-string.py
@@ -39,3 +39,40 @@ class Solution:
             if leftMin < 0:  # required because -> s = ( * ) (
                 leftMin = 0
         return leftMin == 0
+
+
+
+
+
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        start_paren = []
+        star_paren = []
+
+        for i, char in enumerate(s):
+            if char == "(":
+                start_paren.append(i)
+            elif char == "*":
+                star_paren.append(i)
+            else:
+                if start_paren:
+                    start_paren.pop()
+                elif star_paren:
+                    star_paren.pop()
+                else:
+                    return False
+            
+        while start_paren and star_paren:
+            if start_paren[-1] > star_paren[-1]:
+                return False
+            
+            start_paren.pop()
+            star_paren.pop()
+        
+        if start_paren:
+            return False
+        else:
+            return True
+
+                
+        
