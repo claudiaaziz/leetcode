3cd8033f3cc6b6f34b1ae37f5948799869d5ead0
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:13:55 -0400

    846

--- solutions-neetcode/python/0846-hand-of-straights.py
+++ solutions-neetcode/python/0846-hand-of-straights.py
@@ -20,3 +20,41 @@ class Solution:
                         return False
                     heapq.heappop(minH)
         return True
+
+
+
+from collections import Counter
+import heapq
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        hand_dict = Counter(hand)
+        min_heap = list(hand_dict.keys())
+        heapq.heapify(min_heap)
+
+        while min_heap:
+            first = min_heap[0]
+
+            for i in range(first, first + groupSize):
+                if i not in hand_dict:
+                    return False
+                
+                hand_dict[i] -= 1
+                if hand_dict[i] == 0:
+                    # If i isn't the minimum, there will be another minimum and
+                    # i will cause a gap, so we can't complete consecutive numbers.
+                    if i != min_heap[0]:
+                        return False
+                    heapq.heappop(min_heap)
+        
+        return True
+
+
+
+        
+
+
+
+        
