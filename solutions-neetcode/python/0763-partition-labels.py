983110d6bed589638c50b99f6a0237f5f055c78e
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:14:36 -0400

    763

--- solutions-neetcode/python/0763-partition-labels.py
+++ solutions-neetcode/python/0763-partition-labels.py
@@ -19,3 +19,30 @@ class Solution:
                 curLen = 0
             i += 1
         return res
+
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        last_idx_dict = {}
+
+        # Get the last index of each char
+        for i, char in enumerate(s):
+            last_idx_dict[char] = i
+        
+        # Keep track of the ending idx, size, and res
+        end = 0
+        size = 0
+        res = []
+        for i, char in enumerate(s):
+            size += 1
+            end = max(end, last_idx_dict[char])
+
+            # If i is equal to end, we have reached a partition
+            if i == end:
+                res.append(size)
+                size = 0
+        
+        return res
+
+        
