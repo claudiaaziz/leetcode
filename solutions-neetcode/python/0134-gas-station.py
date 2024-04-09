4e1ba22e3b965d22fba5d7adb697132942c2333c
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:13:13 -0400

    134

--- solutions-neetcode/python/0134-gas-station.py
+++ solutions-neetcode/python/0134-gas-station.py
@@ -12,3 +12,25 @@ class Solution:
             total += gas[end] - cost[end]
             end += 1
         return -1
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        
+        # This works because the starting_point is positive, which contributes
+        # to the answer. If total was positive and became negative at any point,
+        # it means that sequence doesn't work. Therefore, a new sequence needs to work
+        # because there exists 1 answer. 
+        total = 0
+        starting_point = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                starting_point = i + 1
+        
+        return starting_point
+        
