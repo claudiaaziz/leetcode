5e45b90fede5541e062d188869a53e8c9702b1cb
Harrison Liang <liangharrison123@gmail.com>
2024-04-09 16:14:17 -0400

    1899

--- solutions-neetcode/python/1899-merge-triplets-to-form-target-triplet.py
+++ solutions-neetcode/python/1899-merge-triplets-to-form-target-triplet.py
@@ -9,3 +9,27 @@ class Solution:
                 if v == target[i]:
                     good.add(i)
         return len(good) == 3
+
+
+
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        good_set = set()
+
+        for sublist in triplets:
+            # If any of the numbers in the sublist is greater than target, we can skip
+            # that sublist because max will be bigger than target.
+            if sublist[0] > target[0] or sublist[1] > target[1] or sublist[2] > target[2]:
+                continue
+            
+            # if a number in a good sublist is equal to a number in the target,
+            # add the index to the set because it works. 
+            for i, num in enumerate(sublist):
+                if sublist[i] == target[i]:
+                    good_set.add(i)
+        
+        # The set should be 3 numbers(0, 1, 2)
+        return len(good_set) == 3
+
+
+        
