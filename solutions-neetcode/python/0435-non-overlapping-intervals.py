571a7df3b84cf5db6aac47814b2996645fe321df
Harrison Liang <liangharrison123@gmail.com>
2024-04-10 20:28:06 -0400

    435

--- solutions-neetcode/python/0435-non-overlapping-intervals.py
+++ solutions-neetcode/python/0435-non-overlapping-intervals.py
@@ -10,3 +10,27 @@ class Solution:
                 res += 1
                 prevEnd = min(end, prevEnd)
         return res
+
+
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        # Sort by start
+        intervals.sort()
+        count = 0
+
+        # Initialize last_end as the first pair's end
+        last_end = intervals[0][1]
+
+        for start, end in intervals[1:]:
+            # If they don't overlap, set the last_end to the current end.
+            if start >= last_end:
+                last_end = end
+            else:
+                # If they do overlap, increment count and set the last_end to the
+                # shorter end, because we "removed" the longer end.
+                count+=1
+                last_end = min(last_end, end)
+
+        return count
+        
