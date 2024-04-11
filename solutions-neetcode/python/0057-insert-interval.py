0ef2efdcdab1bcea8206532eeb06943e08980bd5
Harrison Liang <liangharrison123@gmail.com>
2024-04-10 20:27:26 -0400

    57

--- solutions-neetcode/python/0057-insert-interval.py
+++ solutions-neetcode/python/0057-insert-interval.py
@@ -17,3 +17,34 @@ class Solution:
                 ]
         res.append(newInterval)
         return res
+
+
+
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
+        # 3 conditions to consider:
+
+        # 1. Intervals that come strictly before the newInterval
+        for i in range(len(intervals)):
+            if intervals[i][1] < newInterval[0]:
+                res.append(intervals[i])
+
+        # 2. Intervals that merge with the newInterval
+        for j in range(len(intervals)):
+            if intervals[j][0] <= newInterval[1] and newInterval[0] <= intervals[j][1]:
+                newInterval[0] = min(intervals[j][0], newInterval[0])
+                newInterval[1] = max(intervals[j][1], newInterval[1])
+        res.append(newInterval)
+        
+        # 3. Intervals that come strictly after the newInterval
+        for k in range(len(intervals)):
+            if intervals[k][0] > newInterval[1]:
+                res.append(intervals[k])
+    
+        return res
+
+
+
+                
+        
