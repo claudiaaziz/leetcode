f5a802849a3cc175dd0dc3f69bf20ab751e55d6d
Harrison Liang <liangharrison123@gmail.com>
2024-04-10 20:27:45 -0400

    56

--- solutions-neetcode/python/0056-merge-intervals.py
+++ solutions-neetcode/python/0056-merge-intervals.py
@@ -12,3 +12,30 @@ class Solution:
             else:
                 output.append([start, end])
         return output
+
+
+
+
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Sort the intervals based on start
+        intervals.sort(key = lambda pair: pair[0])
+        # Add the first element to res
+        res = [intervals[0]]
+
+        for start, end in intervals:
+            last_end = res[-1][1]
+
+            # Needs to be merged and end needs to be updated. Start is always
+            # minimized because intervals is sorted. 
+            if last_end >= start:
+                res[-1][1] = max(last_end, end)
+            else:
+                # No need to merge
+                res.append([start, end])
+
+        return res
+
+
+
+        
