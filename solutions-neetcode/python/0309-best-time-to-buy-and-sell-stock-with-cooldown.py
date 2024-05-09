8bc7e44029febcb48b9f9cd2c90bd67d414ddc10
Harrison Liang <liangharrison123@gmail.com>
2024-05-09 11:55:46 -0400

    309

--- solutions-neetcode/python/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ solutions-neetcode/python/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -22,3 +22,43 @@ class Solution:
             return dp[(i, buying)]
 
         return dfs(0, True)
+
+
+
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        memo = {}
+
+        def dfs(i, buying):
+            # Base case if index is out of bounds
+            if i >= len(prices):
+                return 0
+            if (i, buying) in memo:
+                return memo[(i, buying)]
+            
+            # If buying is true
+            if buying:
+                # Buy the stock and set buying to false
+                buy = dfs(i + 1, not buying) - prices[i]
+                # Don't buy the stock and keep buying as true
+                cooldown = dfs(i + 1, buying)
+                # Return the max of the 2 decisions
+                memo[(i, buying)] = max(buy, cooldown)
+            # If buying is false
+            else:
+                # Sell the stock and set buying to true. If you choose to sell,
+                # there's a 1 day cooling period, which is why it's i + 2
+                sell = dfs(i + 2, not buying) + prices[i]
+                # Don't see the stock and keep buying as false
+                cooldown = dfs(i + 1, buying)
+                # Return the max of the 2 decisions
+                memo[(i, buying)] = max(sell, cooldown)
+        
+            return memo[(i, buying)]
+
+        # Start at first price and set buying to true. From there, we can choose
+        # to buy or not buy
+        return dfs(0, True)
