adf627648c33f52d2f509b5924d0029f97d307dd
Harrison Liang <liangharrison123@gmail.com>
2024-05-13 11:43:31 -0400

    494

--- solutions-neetcode/python/0494-target-sum.py
+++ solutions-neetcode/python/0494-target-sum.py
@@ -14,3 +14,27 @@ class Solution:
             return dp[(i, total)]
 
         return backtrack(0, 0)
+
+
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo = {}
+
+        def dfs(i, amt):
+            if (i, amt) in memo:
+                return memo[(i, amt)]
+            if i == len(nums) and amt == target:
+                return 1
+            if i == len(nums) and (amt > target or amt < target):
+                return 0
+            
+            plus = dfs(i + 1, amt + nums[i])
+            minus = dfs(i + 1, amt - nums[i])
+
+            memo[(i, amt)] = plus + minus
+            return plus + minus
+        
+        return dfs(0, 0)
+        
