8e3c58fba8e05c0042b5277f2e0211650126fe93
Harrison Liang <liangharrison123@gmail.com>
2024-05-17 11:34:41 -0400

    329

--- solutions-neetcode/python/0329-longest-increasing-path-in-a-matrix.py
+++ solutions-neetcode/python/0329-longest-increasing-path-in-a-matrix.py
@@ -21,3 +21,49 @@ class Solution:
             for c in range(COLS):
                 dfs(r, c, -1)
         return max(dp.values())
+
+
+
+
+
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        # Get the dimensions of the matrix
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        # Memoize to make it more efficient
+        memo = {}
+
+        # Helper to calculate longest path
+        def dfs(r, c, prev_val):
+            # Return 0 if any of these are true
+            if (r < 0 or r >= rows or 
+                c < 0 or c >= cols or 
+                matrix[r][c] <= prev_val):
+                return 0
+            # Return the memo if it's in there
+            if (r, c) in memo:
+                return memo[(r, c)]
+            
+            # Get the max of the four paths
+            count = 1
+            count = max(count, 
+                1 + dfs(r + 1, c, matrix[r][c]), 
+                1 + dfs(r - 1, c, matrix[r][c]), 
+                1 + dfs(r, c + 1, matrix[r][c]), 
+                1 + dfs(r, c - 1, matrix[r][c]))
+            memo[(r, c)] = count
+
+            # Return the max at that position
+            return count
+            
+        for row in range(rows):
+            for col in range(cols):
+                # Go through every position, some are fast because it got memoized
+                dfs(row, col, -1)
+        
+        # Return the max of the memoized values
+        return max(memo.values())
+        
